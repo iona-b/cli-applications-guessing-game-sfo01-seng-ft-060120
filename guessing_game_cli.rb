0a2b0bb66323ef28049930b677d86d0583fe94ca
Iona <ionabrabender@gmail.com>
2020-05-19 22:02:18 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,9 @@
+require 'pry'
+
 def run_guessing_game
   random_number = rand(1..6)
   user_input = gets
+  binding.pry
   if user_input == random_number
     puts "You guessed the correct number!"
   elsif user_input == "exit"
