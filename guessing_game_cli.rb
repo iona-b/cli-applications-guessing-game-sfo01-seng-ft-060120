40bcc4a11a939408e54d0e884da0be60aadb9d3a
Iona <ionabrabender@gmail.com>
2020-05-19 22:14:08 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,8 @@
 def run_guessing_game
+  
   random_number = rand(1..6)
   user_input = gets.chomp
+  
   if user_input == random_number
     puts "You guessed the correct number!"
   elsif user_input == "exit"
@@ -8,4 +10,5 @@ def run_guessing_game
   else
     puts "Sorry! The computer guessed #{random_number}."
   end
+  
 end
