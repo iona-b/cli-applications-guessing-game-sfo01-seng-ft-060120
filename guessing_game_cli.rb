7a12dac8c85a68e6dc665dc26049adf45dd465a2
Iona <ionabrabender@gmail.com>
2020-05-19 22:12:28 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game
   random_number = rand(1..6)
   user_input = gets.chomp
-  if user_input == random_number.to_s
+  if user_input == random_number
     puts "You guessed the correct number!"
   elsif user_input == "exit"
     puts "Goodbye!"
