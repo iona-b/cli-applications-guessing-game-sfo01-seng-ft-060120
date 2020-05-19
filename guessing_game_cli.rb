63c8d772fb1252f2fb50cd8144978715f1feef55
Iona <ionabrabender@gmail.com>
2020-05-19 22:16:48 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,9 +3,9 @@ def run_guessing_game
   random_number = rand(1..6)
   
   print "Type in your guess: "
-  user_input = gets.chomp
+  user_num = gets.chomp
   
-  if random_number == user_input
+  if user_input == random_number
     puts "You guessed the correct number!"
   elsif user_input == "exit"
     puts "Goodbye!"
